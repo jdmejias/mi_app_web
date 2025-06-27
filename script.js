081cf9ceb3dd2447b2ccdce92dec74b02afba7fb
jdmejias <jhonbbjhon@gmail.com>
2025-06-27 09:14:53 -0500

    version 1.0

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.getElementById('btnClick').addEventListener('click', function() {
     document.getElementById('output').textContent = '¡Botón clicado!';
 });
 
-//Este es un metodo con mucha ham,bre
+//Este es un metodo con mucha ham,bre, suscribete a la tecnologia avanza.
 document.getElementById('contactForm').addEventListener('submit', function(event) {
     event.preventDefault();
     var name = document.getElementById('name').value;
